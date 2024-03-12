715a3263cb605de0ea213cf43d4af909b32f6770
joselinecedano <joselinecedano0@gmail.com>
2024-03-12 01:26:43 -0400

    individual routes showing data

--- strapixreact/react-frontend/src/pages/ReviewDetails.js
+++ strapixreact/react-frontend/src/pages/ReviewDetails.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
 const ReviewDetails = () => {
-  return (
-    <div>Review Details</div>
-  )
-}
+  const { id } = useParams();
+  const { loading, error, data } = useFetch(
+    `http://localhost:1337/api/reviews/${id}`
+  );
 
-export default ReviewDetails
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>Error 😔</p>;
+  }
+
+  console.log(data);
+
+  return (
+    <div className="review-card">
+      <div className="rating">{data.data.attributes.rating}</div>
+      <h2>{data.data.attributes.title}</h2>
+
+      <small>console list</small>
+
+      <p>{data.data.attributes.body[0].children[0].text}</p>
+    </div>
+  );
+};
+
+export default ReviewDetails;
