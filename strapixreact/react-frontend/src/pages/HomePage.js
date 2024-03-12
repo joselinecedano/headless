a3a02aafcb2fd069223ea2c403eab717c5236c7e
joselinecedano <joselinecedano0@gmail.com>
2024-03-12 01:14:40 -0400

    can now see data pulled from strapi backend

--- strapixreact/react-frontend/src/pages/HomePage.js
+++ strapixreact/react-frontend/src/pages/HomePage.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
 const Homepage = () => {
-  return (
-    <div>Home page</div>
-  )
-}
+  const { loading, error, data } = useFetch(
+    "http://localhost:1337/api/reviews"
+  );
 
-export default Homepage
+  if (loading) {
+    return <p> Loading... </p>;
+  }
+  if (error) {
+    return <p> Error 😔 </p>;
+  }
+
+  console.log(data);
+
+  return (
+    <div>
+      {data.data.map((review) => (
+        <div key={review.id} className="review-card">
+          <div className="rating">{review.attributes.rating}</div>
+          <h2>{review.attributes.title}</h2>
+
+          <small>console list</small>
+
+          <p>
+            {review.attributes.body[0].children[0].text.substring(0, 200)} ...
+          </p>
+
+          <Link to={`/details/${review.id}`}>Read more</Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default Homepage;
